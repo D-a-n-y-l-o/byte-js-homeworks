dedcd688e74b06403e09bb40ad2ea3df242fc7be
Danylo Boiko <danvb21@gmail.com>
2022-12-03 16:32:03 +0300

    add homework_2

---
+++ homework_2/script.js
@@ -0,0 +1,19 @@
+// Task #1;
+
+let userAge = 22;
+let userName = "Adam";
+let userSurname = "Smith";
+
+// Task #2;
+
+let newUser;
+let shoppingList;
+let userIsOnline;
+let employeeSalary;
+let cursorCoordinates;
+
+// Task #3;
+
+console.log("Hello World!")
+console.log("Age is", userAge)
+console.log("Age is " + userAge)
